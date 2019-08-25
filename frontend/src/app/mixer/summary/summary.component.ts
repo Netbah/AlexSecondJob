5f6b29e278e41829129def7914aadab7b23ac6c2
F24\alexander.kovalchuk <alexander.kovalchuk@f24.com>
2019-08-25 23:44:23 +0200

    another changes to add funcionality to addToCard button

--- frontend/src/app/mixer/summary/summary.component.ts
+++ frontend/src/app/mixer/summary/summary.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { IIngredient } from '../../models/IIngredient';
 import { IngredientService } from '../services/ingredient.service';
+import { CartService } from 'src/app/services/cart.service';
+import { ProductService } from 'src/app/services/product.service';
+import { ProductDto } from 'src/app/models/ProductDto';
 
 @Component({
   selector: 'app-summary',
@@ -8,17 +11,54 @@ import { IngredientService } from '../services/ingredient.service';
   styleUrls: ['./summary.component.scss']
 })
 export class SummaryComponent implements OnInit {
+  ingredients: IIngredient[]
   price: number;
 
-  constructor(private ingredientService: IngredientService) { }
+  constructor(private ingredientService: IngredientService, private cartService: CartService, private productService: ProductService) { }
 
   ngOnInit() {
     this.ingredientService.$ingridients.subscribe((items: IIngredient[]) => {
+      this.ingredients = items;
       const array = items.map(i => i.price);
       this.price = array.reduce((total, num) => {
         return total + num;
       }, 0)
     })
   }
+
+  delete(item) {
+    this.ingredientService.remove(item);
+  }
+
+ async addToCard(){
+   const newProduct:ProductDto = {
+    "name": "Our new Mixed product",
+    "type": "simple",
+    "regular_price": `${this.price}`,
+    "description": "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.",
+    "short_description": "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
+    "categories": [
+      {
+        "id": 9
+      },
+      {
+        "id": 14
+      }
+    ],
+    "images": [
+      {
+        "src": "http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_2_front.jpg",
+        "position": 0
+      },
+      {
+        "src": "http://demo.woothemes.com/woocommerce/wp-content/uploads/sites/56/2013/06/T_2_back.jpg",
+        "position": 1
+      }
+    ]
+  };
+    const item:any = await this.productService.create(newProduct)
+    
+    this.cartService.addItemToCart(item.id)
+ }
 
 }
